10dc2da5ad70c7381cc48e66487ccabcb17a7c23
Guillaume Gay <guillaume@damcb.com>
2019-05-08 10:53:23 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -6,43 +6,43 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-DISTNAME = 'invagination'
-DESCRIPTION = 'mesoderm invagination modeling based on tyssue'
-MAINTAINER = 'Guillaume Gay'
-MAINTAINER_EMAIL = 'guillaume@damcb.com'
-URL = 'https://github.com/glyg/leg-disk'
-LICENSE = 'MPL-2.0'
-DOWNLOAD_URL = 'https://github.com/damcb/invagination.git'
+DISTNAME = "invagination"
+DESCRIPTION = "mesoderm invagination modeling based on tyssue"
+MAINTAINER = "Guillaume Gay"
+MAINTAINER_EMAIL = "guillaume@damcb.com"
+URL = "https://github.com/suzannelab/invagination"
+LICENSE = "GPL-v3.0"
+DOWNLOAD_URL = "https://github.com/suzannelab/invagination.git"
 
-files = ['notebooks/*.*', 'data/*.*']
+files = ["notebooks/*.*", "data/*.*"]
 
 ## Version management copied form numpy
 ## Thanks to them!
-MAJOR               = 0
-MINOR               = 0
-MICRO               = 1
-ISRELEASED          = False
-VERSION             = '%d.%d.%s' % (MAJOR, MINOR, MICRO)
+MAJOR = 0
+MINOR = 0
+MICRO = 2
+ISRELEASED = True
+VERSION = f"{MAJOR}.{MINOR}.{MICRO}"
 
 
 def git_version():
     def _minimal_ext_cmd(cmd):
         # construct minimal environment
         env = {}
-        for k in ['SYSTEMROOT', 'PATH']:
+        for k in ["SYSTEMROOT", "PATH"]:
             v = os.environ.get(k)
             if v is not None:
                 env[k] = v
         # LANGUAGE is used on win32
-        env['LANGUAGE'] = 'C'
-        env['LANG'] = 'C'
-        env['LC_ALL'] = 'C'
-        out = subprocess.Popen(cmd, stdout = subprocess.PIPE, env=env).communicate()[0]
+        env["LANGUAGE"] = "C"
+        env["LANG"] = "C"
+        env["LC_ALL"] = "C"
+        out = subprocess.Popen(cmd, stdout=subprocess.PIPE, env=env).communicate()[0]
         return out
 
     try:
-        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
-        GIT_REVISION = out.strip().decode('ascii')
+        out = _minimal_ext_cmd(["git", "rev-parse", "HEAD"])
+        GIT_REVISION = out.strip().decode("ascii")
     except OSError:
         GIT_REVISION = "Unknown"
 
@@ -53,28 +53,30 @@ def get_version_info():
     # Adding the git rev number needs to be done inside write_version_py(),
     # otherwise the import of tyssue.version messes up the build under Python 3.
     FULLVERSION = VERSION
-    if os.path.exists('.git'):
+    if os.path.exists(".git"):
         GIT_REVISION = git_version()
-    elif os.path.exists('invagination/version.py'):
+    elif os.path.exists("invagination/version.py"):
         # must be a source distribution, use existing version file
         try:
             from numpy.version import git_revision as GIT_REVISION
         except ImportError:
-            raise ImportError("Unable to import git_revision. Try removing " \
-                              "numpy/version.py and the build directory " \
-                              "before building.")
+            raise ImportError(
+                "Unable to import git_revision. Try removing "
+                "numpy/version.py and the build directory "
+                "before building."
+            )
     else:
         GIT_REVISION = "Unknown"
 
     if not ISRELEASED:
-        FULLVERSION += '.dev0+' + GIT_REVISION[:7]
+        FULLVERSION += ".dev0+" + GIT_REVISION[:7]
 
     return FULLVERSION, GIT_REVISION
 
 
-def write_version_py(filename='invagination/version.py'):
+def write_version_py(filename="invagination/version.py"):
     cnt = """
-# THIS FILE IS GENERATED FROM tyssue SETUP.PY
+# THIS FILE IS GENERATED FROM invagination SETUP.PY
 #
 short_version = '%(version)s'
 version = '%(version)s'
@@ -86,14 +88,20 @@ if not release:
 """
     FULLVERSION, GIT_REVISION = get_version_info()
 
-    a = open(filename, 'w')
+    a = open(filename, "w")
     try:
-        a.write(cnt % {'version': VERSION,
-                       'full_version' : FULLVERSION,
-                       'git_revision' : GIT_REVISION,
-                       'isrelease': str(ISRELEASED)})
+        a.write(
+            cnt
+            % {
+                "version": VERSION,
+                "full_version": FULLVERSION,
+                "git_revision": GIT_REVISION,
+                "isrelease": str(ISRELEASED),
+            }
+        )
     finally:
         a.close()
+
 
 if __name__ == "__main__":
 
@@ -107,20 +115,20 @@ if __name__ == "__main__":
         license=LICENSE,
         download_url=DOWNLOAD_URL,
         version=VERSION,
-        classifiers=["Development Status :: 4 - Beta",
-                     "Intended Audience :: Science/Research",
-                     "License :: OSI Approved :: MPL v2.0",
-                     "Natural Language :: English",
-                     "Operating System :: MacOS",
-                     "Operating System :: Microsoft",
-                     "Operating System :: POSIX :: Linux",
-                     "Programming Language :: Python :: 3.6",
-                     "Topic :: Scientific/Engineering :: Bio-Informatics",
-                     "Topic :: Scientific/Engineering :: Medical Science Apps",
-                     ],
-
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: MPL v2.0",
+            "Natural Language :: English",
+            "Operating System :: MacOS",
+            "Operating System :: Microsoft",
+            "Operating System :: POSIX :: Linux",
+            "Programming Language :: Python :: 3.6",
+            "Topic :: Scientific/Engineering :: Bio-Informatics",
+            "Topic :: Scientific/Engineering :: Medical Science Apps",
+        ],
         packages=find_packages(),
-        package_data={'invagination': files},
+        package_data={"invagination": files},
         include_package_data=True,
-        zip_safe=False
+        zip_safe=False,
     )
