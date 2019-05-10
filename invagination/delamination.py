4870f6d68003f14dc2113b064d5187be0ec813cf
Sophie THEIS <sophiets13@gmail.com>
2019-05-10 14:27:51 +0200

    Refactoring code in notebook
    refactoring code to simplify notebook.
    To do : check smallEllipsoidInvagination
               : comment

--- invagination/delamination.py
+++ invagination/delamination.py
@@ -2,9 +2,20 @@
 Delamination behavior in a 2.5D sheet
 """
 
+import os
 import random
 import numpy as np
 
+from invagination.ellipsoid import (RadialTension,
+                                    VitellineElasticity)
+
+from tyssue.io import hdf5
+from tyssue.dynamics import effectors
+from tyssue.dynamics.factory import model_factory
+from tyssue.geometry.sheet_geometry import EllipsoidGeometry as geom
+from tyssue.solvers.sheet_vertex_solver import Solver
+from tyssue.behaviors.event_manager import EventManager
+from tyssue.behaviors.sheet.basic_events import check_tri_faces
 from tyssue.behaviors.sheet.delamination_events import (constriction)
 
 
@@ -18,7 +29,8 @@ def check_enter_in_process(
     """
     for f in mesoderm:
         if enter_in_process(sheet, f, base, amp, largeur, density_proba):
-            manager.append(constriction, face_id=f, **sheet.settings["delamination"])
+            manager.append(constriction, face_id=f, **
+                           sheet.settings["delamination"])
 
 
 def gaussian(x, base=0, amp=0.8, width=0.7, n=2):
@@ -54,21 +66,178 @@ def enter_in_process(sheet, f, base=0.003, amp=0.5, width=2.5, n=6):
     return False
 
 
-def define_mesoderm(sheet, a=145.0, b=40.0, coords=["x", "z", "y"]):
-    """
-    Define an oval area that will become the mesoderm.
-    a: radius on the first-axis
-    b: radius on the second-axis
-    """
-    x, y, z = coords
-    x_ = sheet.face_df[x] / b
-    y_ = sheet.face_df[y] / a
+model = model_factory(
+    [
+        RadialTension,
+        VitellineElasticity,
+        effectors.FaceContractility,
+        effectors.FaceAreaElasticity,
+        # effectors.CellVolumeElasticity,
+        effectors.LumenVolumeElasticity,
+    ], effectors.FaceAreaElasticity)
 
-    radius = x_ ** 2 + y_ ** 2
-    height = sheet.face_df[z]
 
-    sheet.face_df["is_mesoderm"] = (radius <= 1) & (height > 0)
+def delamination_process(sim_save_dir, sheet, max_contractility_rate, critical_area,
+                         radial_tension, nb_iteraction_max,
+                         profile_width, k, iteration,
+                         cable_cut=False, apical_cut=False, nb_apical_cut=2):
+    # Directory definition
+    dirname = '{}_contractility_{}_critical_area_{}_radialtension_{}'.format(
+        max_contractility_rate, critical_area, radial_tension, iteration)
+    dirname = os.path.join(sim_save_dir, dirname)
 
-    # Remove not active face
-    face_not_active = sheet.edge_df[~sheet.edge_df.is_active.astype(bool)].face.unique()
-    sheet.face_df.loc[face_not_active, "is_mesoderm"] = False
+    print('starting {}'.format(dirname))
+    try:
+        os.mkdir(dirname)
+    except IOError:
+        pass
+
+    settings = {'critical_area': critical_area,
+                'radial_tension': radial_tension,
+                'nb_iteration': 0,
+                'nb_iteration_max': nb_iteraction_max,
+                'contract_neighbors': True,
+                'critical_area_neighbors': 12,
+                'contract_span': 3,
+                'basal_contract_rate': 1.001,
+                'geom': geom,
+                'contraction_column': 'contractility'}
+
+    # Add some information to the sheet
+    sheet2 = sheet.copy(deep_copy=True)
+    sheet2.face_df['id'] = sheet2.face_df.index.values
+    sheet2.settings['delamination'] = settings
+    settings2 = {'critical_length': 0.3}
+    sheet2.settings['T1'] = settings2
+
+    #""" Initiale find minimal energy
+    # To be sure we are at the equilibrium
+    solver = Solver
+    solver_kw = {'minimize': {'method': 'L-BFGS-B',
+                              'options': {'ftol': 1e-8,
+                                          'gtol': 1e-8}}}
+    res = solver.find_energy_min(sheet2, geom, model, **solver_kw)
+
+    sheet2 = run_sim(dirname, solver, solver_kw, sheet2, geom, model,
+                     max_contractility_rate, profile_width, k,
+                     cable_cut, apical_cut, nb_apical_cut)
+
+    print('{} done'.format(dirname))
+    print('~~~~~~~~~~~~~~~~~~~~~\n')
+
+
+def run_sim(dirname, solver, solver_kw, sheet, geom, model,
+            max_contractility_rate, profile_width, k,
+            cable_cut=False, apical_cut=False, nb_apical_cut=2):
+
+    # Initiate manager
+    manager = EventManager('face')
+    sheet.face_df['enter_in_process'] = 0
+
+    t = 0
+    stop = 200
+    sheet.face_df['contract_rate'] = 0
+
+    if apical_cut:
+        if nb_apical_cut == 1:
+            # posterior apical ablation
+            z_ablat = 45.
+
+            srce_z = sheet.upcast_srce(sheet.vert_df['z'])
+            trgt_z = sheet.upcast_trgt(sheet.vert_df['z'])
+            srce_y = sheet.upcast_srce(sheet.vert_df['y'])
+            trgt_y = sheet.upcast_trgt(sheet.vert_df['y'])
+
+            meso_edge = sheet.upcast_face(sheet.face_df['is_mesoderm'])
+            cut_edges = sheet.edge_df[(((srce_z < z_ablat) & (trgt_z >= z_ablat))
+                                       | ((srce_z >= z_ablat) & (trgt_z <= z_ablat)))
+                                      & meso_edge & ((srce_y > 0) & (trgt_y > 0))]
+            cut_faces = cut_edges['face'].unique()
+            sheet.face_df.loc[cut_faces, [
+                'contractility', 'area_elasticity']] = 1e-2
+
+        elif nb_apical_cut == 2:
+            # anterior & posterior apical ablation
+            z_ablat_post = 45.
+            z_ablat_ant = -45.
+
+            srce_z = sheet.upcast_srce(sheet.vert_df['z'])
+            trgt_z = sheet.upcast_trgt(sheet.vert_df['z'])
+            srce_y = sheet.upcast_srce(sheet.vert_df['y'])
+            trgt_y = sheet.upcast_trgt(sheet.vert_df['y'])
+
+            meso_edge = sheet.upcast_face(sheet.face_df['is_mesoderm'])
+            cut_edges = sheet.edge_df[(((srce_z < z_ablat_post) & (trgt_z >= z_ablat_post))
+                                       | ((srce_z >= z_ablat_post) & (trgt_z <= z_ablat_post))
+                                       | ((srce_z < z_ablat_ant) & (trgt_z >= z_ablat_ant))
+                                       | ((srce_z >= z_ablat_ant) & (trgt_z <= z_ablat_ant)))
+                                      & meso_edge & ((srce_y > 0) & (trgt_y > 0))]
+            cut_faces = cut_edges['face'].unique()
+            sheet.face_df.loc[cut_faces, [
+                'contractility', 'area_elasticity']] = 1e-2
+            sheet.face_df.loc[cut_faces, 'is_mesoderm'] = False
+
+    # Add all cells in constriction process
+    for f in sheet.face_df[sheet.face_df['is_mesoderm']].index:
+        x = sheet.face_df.loc[f, 'x']
+        c_rate = 1 + (max_contractility_rate - 1) * ((1 + np.exp(-k * profile_width)) /
+                                                     (1 + np.exp(k * (abs(x) - profile_width))))
+
+        sheet.face_df.loc[f, 'contract_rate'] = c_rate
+        delam_kwargs = sheet.settings["delamination"].copy()
+        delam_kwargs.update(
+            {
+                'face_id': f,
+                'contract_rate': c_rate,
+                'current_traction': 0,
+                'max_traction': 30
+            }
+        )
+        manager.append(constriction, **delam_kwargs)
+
+    for f in sheet.face_df[sheet.face_df['is_relaxation']].index:
+        delam_kwargs = sheet.settings["delamination"].copy()
+        delam_kwargs.update(
+            {
+                'face_id': f,
+                'contract_rate': max_contractility_rate,
+                'current_traction': 0,
+                'max_traction': 30
+            }
+        )
+        manager.append(constriction, **delam_kwargs)
+
+    while manager.current and t < stop:
+        # Clean radial tension on all vertices
+        sheet.vert_df['radial_tension'] = 0
+        manager.execute(sheet)
+
+        if cable_cut:
+            # Mettre ici la mise à 0 de la force AB dans la zone -45 45
+            sheet.vert_df['radial_tension'] = [0 if ((z > -45.) and (z < 45.))
+                                               else rad
+                                               for (z, rad) in sheet.vert_df[['z', 'radial_tension']].values]
+
+        res = solver.find_energy_min(sheet, geom, model, **solver_kw)
+
+        # add noise on vertex position to avoid local minimal.
+        sheet.vert_df[
+            ['x', 'y']] += np.random.normal(scale=1e-3, size=(sheet.Nv, 2))
+        geom.update_all(sheet)
+
+        figname = os.path.join(
+            dirname, 'invagination_{:04d}.png'.format(t))
+        hdfname = figname[:-3] + 'hf5'
+        hdf5.save_datasets(hdfname, sheet)
+
+        # Add cells with initially 3 neighbourghs to be eliminated
+        check_tri_faces(sheet, manager)
+        # Add T1 transition for face with at least one edge shorter than critical length
+        #[manager.append(type1_transition, f, kwargs=sheet.settings['T1']) for f in sheet.edge_df[
+        # sheet.edge_df['length'] <
+        # sheet.settings['T1']['critical_length']]['face'].unique()]
+
+        manager.update()
+        t += 1
+
+    return sheet
--- invagination/ellipsoid.py
+++ invagination/ellipsoid.py
@@ -1,51 +1,12 @@
+import json
 import numpy as np
 import pandas as pd
 
-from tyssue import SheetGeometry, BulkGeometry, ClosedMonolayerGeometry
+from tyssue import Sheet
+from tyssue.io import hdf5
 from tyssue.utils.utils import _to_3d, to_nd
 from tyssue.dynamics.sheet_gradients import height_grad
 from tyssue.dynamics import units, effectors, model_factory
-
-
-class EllipsoidGeometry(SheetGeometry):
-    """
-    x = a \cos(\phi)
-
-    """
-
-    @staticmethod
-    def update_height(eptm):
-        """see data/svg/ellipsoid_geometry.svg
-        """
-
-        a, b, c = eptm.settings['abc']
-        vitelline_space = eptm.settings['vitelline_space']
-        eptm.vert_df['rho'] = np.linalg.norm(eptm.vert_df[['x', 'y']], axis=1)
-        eptm.vert_df['theta'] = np.arcsin((eptm.vert_df.z / c).clip(-1, 1))
-        eptm.vert_df['phi'] = np.arctan2(eptm.vert_df.y, eptm.vert_df.x)
-        eptm.vert_df['vitelline_rho'] = (
-            a + vitelline_space) * np.cos(eptm.vert_df['theta'])
-
-        eptm.vert_df['basal_shift'] = (eptm.vert_df['vitelline_rho']
-            - vitelline_space - a + eptm.specs['vert']['basal_shift'])
-
-        eptm.vert_df['delta_rho'] = eptm.vert_df.eval(
-            'rho - vitelline_rho').clip(lower=0)
-        SheetGeometry.update_height(eptm)
-
-    @staticmethod
-    def update_vol(eptm):
-
-        if "segment" in eptm.edge_df:
-            for c in 'xyz':
-                eptm.edge_df['c' + c] = 0
-            ClosedMonolayerGeometry.update_vol(eptm)
-        ClosedMonolayerGeometry.update_lumen_vol(eptm)
-
-    @staticmethod
-    def scale(eptm, scale, coords):
-        SheetGeometry.scale(eptm, scale, coords)
-        eptm.settings['abc'] = [u * scale for u in eptm.settings['abc']]
 
 
 class VitellineElasticity(effectors.AbstractEffector):
@@ -59,7 +20,7 @@ class VitellineElasticity(effectors.AbstractEffector):
             'vitelline_K',
             'is_active',
             'delta_rho'}
-        }  # distance to the vitelline membrane
+    }  # distance to the vitelline membrane
 
     @staticmethod
     def energy(eptm):
@@ -80,9 +41,9 @@ class RadialTension(effectors.AbstractEffector):
     magnitude = 'radial_tension'
     label = 'Apical basal tension'
     element = 'vert'
-    specs = {'vert':{'is_active',
-             'height',
-             'radial_tension'}}
+    specs = {'vert': {'is_active',
+                      'height',
+                      'radial_tension'}}
 
     @staticmethod
     def energy(eptm):
@@ -93,16 +54,87 @@ class RadialTension(effectors.AbstractEffector):
     def gradient(eptm):
         grad = height_grad(eptm) * to_nd(
             eptm.vert_df.eval('radial_tension'), 3)
-        grad.columns = ['g'+c for c in eptm.coords]
+        grad.columns = ['g' + c for c in eptm.coords]
         return grad, None
 
 
-model = model_factory(
-    [
-    RadialTension,
-    VitellineElasticity,
-    effectors.FaceContractility,
-    effectors.FaceAreaElasticity,
-    #effectors.CellVolumeElasticity,
-    effectors.LumenVolumeElasticity,
-    ], effectors.FaceAreaElasticity)
+def define_mesoderm(sheet, a=145.0, b=40.0, coords=["x", "z", "y"]):
+    """
+    Define an oval area that will become the mesoderm.
+    a: radius on the first-axis
+    b: radius on the second-axis
+    """
+    x, y, z = coords
+    x_ = sheet.face_df[x] / b
+    y_ = sheet.face_df[y] / a
+
+    radius = x_ ** 2 + y_ ** 2
+    height = sheet.face_df[z]
+
+    sheet.face_df["is_mesoderm"] = (radius <= 1) & (height > 0)
+
+    # Remove not active face
+    face_not_active = sheet.edge_df[
+        ~sheet.edge_df.is_active.astype(bool)].face.unique()
+    sheet.face_df.loc[face_not_active, "is_mesoderm"] = False
+
+
+def define_relaxation_cells(sheet, a=145., b=40.,
+                            aa=145., bb=40.,
+                            coords=['x', 'z', 'y']):
+    """
+    Define 2 lines of cells surronding the mesoderm which will be relaxating
+
+    a: radius on the first-axis
+    b: radius on the second-axis
+    """
+    define_mesoderm(sheet, aa, bb)
+    x, y, z = coords
+    x_ = sheet.face_df[x] / b
+    y_ = sheet.face_df[y] / a
+
+    rayon = x_**2 + y_**2
+    height = sheet.face_df[z]
+
+    sheet.face_df['is_relaxation'] = ~((rayon <= 1) & (height > 0))
+    sheet.face_df['is_relaxation'] = [False if ((not line[1]['is_mesoderm']) & (line[1]['is_relaxation']))
+                                      else line[1]['is_relaxation']
+                                      for line in sheet.face_df.iterrows()]
+
+    sheet.face_df['is_mesoderm'] = [False if ((line[1]['is_mesoderm']) & (line[1]['is_relaxation']))
+                                    else line[1]['is_mesoderm']
+                                    for line in sheet.face_df.iterrows()]
+    # Remove not active face
+    face_not_active = sheet.edge_df[
+        ~sheet.edge_df.is_active.astype(bool)].face.unique()
+    sheet.face_df.loc[face_not_active, 'is_relaxation'] = False
+
+
+def initiate_ellipsoid(dataset_path, json_path):
+    """
+    Create ellipsoid tissue as a sheet with mesodermal cells
+
+    dataset_path: initial hf45 file
+    b: json spec file
+    """
+    dsets = hdf5.load_datasets(dataset_path,
+                               data_names=['vert', 'edge', 'face'])
+
+    with open(json_path, 'r+') as fp:
+        specs = json.load(fp)
+
+    sheet = Sheet('ellipse', dsets, specs)
+
+    # Modify some initial value
+    sheet.settings['threshold_length'] = 1e-3
+    sheet.settings['vitelline_space'] = 0.2
+    sheet.vert_df['radial_tension'] = 0.
+    sheet.settings['lumen_prefered_vol'] = 4539601.384437251
+    sheet.settings['lumen_vol_elasticity'] = 3.e-6
+    sheet.edge_df.cell = np.nan
+
+    # Define mesoderm and relaxating cells
+    define_mesoderm(sheet, 145, 40)
+    define_relaxation_cells(sheet, 140, 33, 145, 40)
+
+    return sheet
